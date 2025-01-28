0c966760d82a40584f3baefa33d6aeb3bb70c9d0
OmarH455 <omarhassann79@gmail.com>
2025-01-29 00:16:02 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // src/config.js
 const config = {
-    BACKEND_URL: "http://127.0.0.1:5000", // Replace with your backend URL
+    BACKEND_URL: "LocalHost
+        :5000", // Replace with your backend URL
   };
   
   export default config;
